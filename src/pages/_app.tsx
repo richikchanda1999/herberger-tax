7331f69c6fc7ee20c3f1fc5785a1442d182dda18
Richik Chanda <richikchanda1999@gmail.com>
2023-02-18 21:33:41 +0530

    chore: ui WIP

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,6 +1,10 @@
 import 'src/styles/globals.css'
 import type { AppProps } from 'next/app'
+import { ChakraBaseProvider } from '@chakra-ui/react'
+import theme from 'src/theme'
 
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return <ChakraBaseProvider theme={theme}>
+    <Component {...pageProps} />
+  </ChakraBaseProvider>
 }
